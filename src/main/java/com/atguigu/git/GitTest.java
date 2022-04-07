033ae36a2635eb317b686a22c2c66359aef6c758
yespengke <yespengke@163.com>
2022-04-07 13:22:52 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
     }
 
 }
